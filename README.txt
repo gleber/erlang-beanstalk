51e281506f409e7f7ea5c6e16e52e4f22331a652
Tim Fletcher <tim@tfletcher.com>
2008-04-27 10:19:24 +0100

    First commit.

---
+++ README.txt
@@ -0,0 +1,60 @@
+================
+Erlang-Beanstalk
+================
+
+
+What is this?
+-------------
+
+An Erlang module that wraps the beanstalkd protocol.
+
+
+What is beanstalkd?
+-------------------
+
+A fast, distributed, in-memory workqueue service.
+
+See http://xph.us/software/beanstalkd/ for more information,
+and links to clients in other languages.
+
+
+How do I use it?
+----------------
+
+First, make sure both of the modules are compiled, and that you
+have an instance of beanstalkd running in the background. Then
+you can connect to it like so (using whatever host and port values
+are appropriate for you):
+
+  {ok, Socket} = beanstalk:connect(_Host="0.0.0.0", _Port=3000).
+
+At the moment this is just a direct call to gen_tcp:connect/3.
+
+Jobs are manipulated using the beanstalk_job module. To create
+a new job, pass a string or a binary to beanstalk_job:new/1:
+
+  Job = beanstalk_job:new("hello").
+
+You can alter the defaults using beanstalk_job:with/3, e.g.,
+
+  DelayedJob = beanstalk_job:with(delay, 30, Job).
+
+All the functions in the beanstalk module (apart from connect/2)
+correspond to calls in the beanstalkd protocol. Each expects to
+be given the socket connection as the last argument. For example:
+
+  beanstalk:put(Job, Socket).
+
+  {reserved, Job} = beanstalk:reserve(Socket).
+
+  deleted = beanstalk:delete(Job, Socket).
+
+And so on.
+
+
+WTF?
+----
+
+Found a bug? Think I could have done something better?
+
+Don't let it trouble you :) Let me know!
---
+++ src/beanstalk_job.erl
@@ -0,0 +1,60 @@
+% Copyright (c) 2008 Tim Fletcher <tfletcher.com>
+% 
+% Permission is hereby granted, free of charge, to any person
+% obtaining a copy of this software and associated documentation
+% files (the "Software"), to deal in the Software without
+% restriction, including without limitation the rights to use,
+% copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the
+% Software is furnished to do so, subject to the following
+% conditions:
+% 
+% The above copyright notice and this permission notice shall be
+% included in all copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+% NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+% HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+% WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+% FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+% OTHER DEALINGS IN THE SOFTWARE.
+
+-module(beanstalk_job).
+
+-export([new/1]).
+-export([new/2]).
+-export([id/1]).
+-export([body/1]).
+-export([priority/1]).
+-export([delay/1]).
+-export([ttr/1]).
+-export([with/3]).
+
+
+new(ID) when is_integer(ID) ->
+  [{id, ID}];
+new(Body) when is_list(Body); is_binary(Body) ->
+  [{body, Body}].
+
+new(ID, Body) ->
+  [{id, ID}, {body, Body}].
+
+id(Job) ->
+  proplists:fetch(id, Job).
+
+body(Job) ->
+  proplists:fetch(body, Job).
+
+priority(Job) ->
+  proplists:fetch(priority, Job, 0).
+
+delay(Job) ->
+  proplists:fetch(delay, Job, 0).
+
+ttr(Job) ->
+  proplists:fetch(ttr, Job, 60).
+
+with(Key, Value, Job) ->
+  [{Key, Value}|proplists:delete(Key, Job)].
