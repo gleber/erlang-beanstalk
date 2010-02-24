e1afdf87f8fb4e1f2009a2c3c0aab7fc143177bb
Gleb Peregud <gleber.p@gmail.com>
2010-02-24 23:13:33 +0100

    fix yaml parser to parse non-"---"-prefixed yaml documents to parse stats-job results (tested with beanstalkd 1.4.3)

--- src/beanstalk_yaml.erl
+++ src/beanstalk_yaml.erl
@@ -9,36 +9,42 @@ test() ->
   {"hello", <<"world">>} = binary_break_at($\n, <<"hello\nworld">>).
 
 parse(<<"---\n", Data/bytes>>) ->
-  case Data of
-    <<"- ", _/bytes>> ->
-      parse_sequence(Data, []);
-    _ ->
-      parse_mapping(Data, [])
-  end.
+    parse_document(Data);
+parse(Data) ->
+    parse_document(Data).
 
-parse_sequence(Data, Sequence) when size(Data) =:= 0 ->
-  lists:reverse(Sequence);
+parse_document(Data) ->
+    case Data of
+        <<"- ", _/bytes>> ->
+            parse_sequence(Data, []);
+        _ ->
+            parse_mapping(Data, [])
+    end.
+
+
+parse_sequence(<<>>, Sequence) ->
+    lists:reverse(Sequence);
 parse_sequence(<<"- ", Data/bytes>>, Sequence) ->
-  {Value, MoreData} = binary_break_at($\n, Data),
-  parse_sequence(MoreData, [Value|Sequence]).
+    {Value, MoreData} = binary_break_at($\n, Data),
+    parse_sequence(MoreData, [Value|Sequence]).
 
-parse_mapping(Data, Mapping) when size(Data) =:= 0 ->
-  lists:reverse(Mapping);
+parse_mapping(<<>>, Mapping) ->
+    lists:reverse(Mapping);
 parse_mapping(Data, Mapping) ->
-  {K, <<" ", Rest/bytes>>} = binary_break_at($:, Data),
-  {V, MoreData} = binary_break_at($\n, Rest),
-  parse_mapping(MoreData, [{K, V}|Mapping]).
+    {K, <<" ", Rest/bytes>>} = binary_break_at($:, Data),
+    {V, MoreData} = binary_break_at($\n, Rest),
+    parse_mapping(MoreData, [{K, V}|Mapping]).
 
 binary_break_at(C, Data) when is_binary(Data) ->
-  binary_break_at(C, Data, []).
+    binary_break_at(C, Data, []).
 
-binary_break_at(_C, Data, Prefix) when is_binary(Data), size(Data) =:= 0 ->
-  {lists:reverse(Prefix), []};
+binary_break_at(_C, <<>>, Prefix) ->
+    {lists:reverse(Prefix), []};
 binary_break_at(C, Data, Prefix) when is_binary(Data) ->
-  <<H, T/bytes>> = Data,
-  case H of
-    C ->
-      {lists:reverse(Prefix), T};
-    _ ->
-      binary_break_at(C, T, [H | Prefix])
-  end.
+    <<H, T/bytes>> = Data,
+    case H of
+        C ->
+            {lists:reverse(Prefix), T};
+        _ ->
+            binary_break_at(C, T, [H | Prefix])
+    end.
