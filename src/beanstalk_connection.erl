0813c78497b6879f9e26869ec8b1448e44e7dc14
Gleb Peregud <gleber.p@gmail.com>
2010-02-24 22:47:39 +0100

    make beanstalk_connection aware of multi-packet parser
    It still assumes that there is exactly one response for one request
    and that there is no OOB data. It could be useful in future...

--- src/beanstalk_connection.erl
+++ src/beanstalk_connection.erl
@@ -4,36 +4,39 @@
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
+-record(state, {socket, buffer = <<>>}).
 
 init({Host, Port, Timeout}) ->
-  case gen_tcp:connect(Host, Port, [binary, {packet, 0}, {active, false}], Timeout) of
-    Reply={ok, _} ->
-      Reply;
-    Error ->
-      {stop, Error}
-  end.
+    case gen_tcp:connect(Host, Port, [binary, {packet, 0}, {active, false}], Timeout) of
+        {ok, Socket} ->
+            {ok, #state{socket = Socket}};
+        Error ->
+            {stop, Error}
+    end.
 
-handle_call({put, Data, Params}, _From, Socket) ->
-  Pri = proplists:get_value(pri, Params, 0),
-  Delay = proplists:get_value(delay, Params, 0),
-  TTR = proplists:get_value(delay, Params, 60),
-  beanstalk_command:send(Socket, {put, Pri, Delay, TTR, size(Data)}, Data),
-  {reply, beanstalk_response:recv(Socket), Socket};
-handle_call(Command, _From, Socket) ->
-  beanstalk_command:send(Socket, Command),
-  {reply, beanstalk_response:recv(Socket), Socket}.
+handle_call({put, Data, Params}, _From, #state{socket = Socket, buffer = Buffer} = State) ->
+    Pri = proplists:get_value(pri, Params, 0),
+    Delay = proplists:get_value(delay, Params, 0),
+    TTR = proplists:get_value(delay, Params, 60),
+    beanstalk_command:send(Socket, {put, Pri, Delay, TTR, size(Data)}, Data),
+    {ok, Reply, <<>> = NewBuffer} = beanstalk_response:recv(Socket, Buffer),
+    {reply, Reply, State#state{buffer = NewBuffer}};
+handle_call(Command, _From, #state{socket = Socket, buffer = Buffer} = State) ->
+    beanstalk_command:send(Socket, Command),
+    {ok, Reply, <<>> = NewBuffer} = beanstalk_response:recv(Socket, Buffer),
+    {reply, Reply, State#state{buffer = NewBuffer}}.
 
 handle_cast({stop}, State) ->
-  {stop, normal, State};
+    {stop, normal, State};
 handle_cast(_Msg, State) ->
-  {noreply, State}.
+    {noreply, State}.
 
 handle_info(_Msg, State) ->
-  {noreply, State}.
+    {noreply, State}.
 
 code_change(_OldVsn, State, _Extra) ->
-  {ok, State}.
+    {ok, State}.
 
 terminate(normal, Socket) ->
-  catch gen_tcp:close(Socket),
-  ok.
+    catch gen_tcp:close(Socket),
+    ok.
